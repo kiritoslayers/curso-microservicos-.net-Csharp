72717993f7117fc90e9ab7e74ea6c01d1bd23c1a
KiritoSlayers <fersilvavillamarin@gmail.com>
2023-08-26 12:07:13 -0300

    Encaminhando alguns arquivos do projeto

--- Modulo_07/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ Modulo_07/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,6 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region CHAT GPT CONNECTION STRING
+
+string connectionString = "Server=localhost; Port=3306; Database=geek_shopping_product_api; Uid=root; Pwd=password;"; // Replace this with your actual connection string
+builder.Services.AddDbContext<MySQLContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+#endregion
 
 var app = builder.Build();
 
---
+++ database.sql
@@ -0,0 +1,4 @@
+-- criar o banco de dados!
+CREATE database geek_shopping_product_api
+
+
